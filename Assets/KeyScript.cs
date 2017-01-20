093674da8a411668697aebaaefc9501c09114443
midasplayer\Edward.James <Edward.James@king.,com>
2017-01-20 20:51:33 +0000

    Added key activation

--- Assets/KeyScript.cs
+++ Assets/KeyScript.cs
@@ -20,15 +20,54 @@ public class KeyState
 {
 	public int octave;
 	public KeyNote keyNote;
-	public Color color;
+	public Color activedColor;
+	public Color deactivatedColor;
 }
 
 public class KeyScript : MonoBehaviour
 {
+	public GameObject keyRayCast;
+
+	private SpriteRenderer spriteRenderer;
+
 	[SerializeField]
 	private KeyState keyState;
 
 	private bool activated = false;
 
+	private void Awake()
+	{
+		spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+	}
 
+	private void Update()
+	{
+		checkForPlayer();
+		renderKeyState();
+	}
+
+	private void checkForPlayer()
+	{
+		RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, keyRayCast.transform.position);
+		if (raycastHit.collider != null)
+		{
+			if (raycastHit.collider.CompareTag("Player"))
+			{
+				activated = true;
+				raycastHit.collider.GetComponent<PlayerScript>().setActiveKey(this);
+			}
+		}
+	}
+
+	private void renderKeyState()
+	{
+		if (activated)
+		{
+			spriteRenderer.color = keyState.activedColor;
+		}
+		else
+		{
+			spriteRenderer.color = keyState.deactivatedColor;
+		}
+	}
 }
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+	private KeyScript keyScript;
+
+	void Update()
+	{
+		float input = Input.GetAxis("Horizontal");
+		if (input != 0)
+		{
+			move(input*Vector3.right);
+		}
+	}
+
+	public void setActiveKey(KeyScript kScript)
+	{
+		keyScript = kScript;
+	}
+	
+	private void move(Vector3 direction)
+	{
+		transform.position += direction;
+
+		keyScript = null;
+	}
+}
