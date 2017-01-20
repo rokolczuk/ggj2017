a876ad86bef08e90ae0d972d48b0799119821b00
HaraldMaassen <coamithra@gmail.com>
2017-01-20 20:10:49 +0000

    Added helpful things

---
+++ Assets/Scripts/Networking/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameManager : NetworkBehaviour
+{
+	private EventDispatcher eventDispatcher;
+	PrefabManager prefabManager;
+
+	public void Awake()
+	{
+		eventDispatcher = FindObjectOfType<EventDispatcher>();
+		prefabManager = FindObjectOfType<PrefabManager>();
+
+		eventDispatcher.Add<ServerAddedPlayer>(OnServerAddedPlayer);
+	}
+
+	// Use this for initialization
+	private void Start()
+	{
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+	}
+
+	private void OnServerAddedPlayer(ServerAddedPlayer eventData)
+	{
+		Ship ship = prefabManager.Instantiate<Ship>();
+		NetworkServer.SpawnWithClientAuthority(ship.gameObject, eventData.Player);
+	}
+}
---
+++ Assets/Scripts/Networking/SpaceKingsNetworkManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerAddedPlayer : IGameEvent
+{
+	public ServerAddedPlayer(GameObject player)
+	{
+		Player = player;
+	}
+
+	public GameObject Player { get; private set; }
+}
+
+public class SpaceKingsNetworkManager : NetworkManager
+{
+	private EventDispatcher eventDispatcher;
+
+	public void Awake()
+	{
+		eventDispatcher = FindObjectOfType<EventDispatcher>();
+	}
+
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+	{
+		GameObject player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+		NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+
+		eventDispatcher.Dispatch(new ServerAddedPlayer(player));
+	}
+}
