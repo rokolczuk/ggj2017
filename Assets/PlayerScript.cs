70d8c5d0a69bcc2d87473f91ccf53f579c44be67
midasplayer\Edward.James <Edward.James@king.,com>
2017-01-20 22:15:18 +0000

    Player keys work

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -2,12 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class SelectedKeyChanged
+{
+	public PlayerScript playerScript;
+	public KeyScript keyScript;
+
+	public SelectedKeyChanged(PlayerScript playerScript, KeyScript keyScript)
+	{
+		this.playerScript = playerScript;
+		this.keyScript = keyScript;
+	}
+}
+
 public class PlayerScript : MonoBehaviour
 {
+	public GameObject keyRayCast;
+
 	private KeyScript keyScript;
 
 	void Update()
 	{
+		Debug.DrawRay(transform.position, keyRayCast.transform.localPosition * Vector3.Distance(transform.position, keyRayCast.transform.position));
+
+		checkForKey();
+
 		float input = Input.GetAxis("Horizontal");
 		if (input != 0)
 		{
@@ -22,16 +40,40 @@ public class PlayerScript : MonoBehaviour
 			}
 		}
 	}
-
-	public void setActiveKey(KeyScript kScript)
-	{
-		keyScript = kScript;
-	}
 	
 	private void move(Vector3 direction)
 	{
 		transform.position += direction;
 
 		keyScript = null;
+	}
+
+	private void checkForKey()
+	{
+		RaycastHit2D[] raycastHit = Physics2D.RaycastAll(transform.position, keyRayCast.transform.localPosition);
+
+		bool newKey = false;
+
+		for (int i = 0; i < raycastHit.Length; i++)
+		{
+			if (raycastHit[i].collider != null)
+			{
+				if (raycastHit[i].collider.CompareTag("Key"))
+				{
+					newKey = true;
+					KeyScript newKeyScript = raycastHit[i].collider.GetComponent<KeyScript>();
+					if (keyScript != newKeyScript)
+					{
+						EventDispatcher.Dispatch(new SelectedKeyChanged(this, newKeyScript));
+						keyScript = newKeyScript;
+					}
+				}
+			}
+		}
+
+		if(!newKey)
+		{
+			EventDispatcher.Dispatch(new SelectedKeyChanged(this, null));
+		}
 	}
 }
