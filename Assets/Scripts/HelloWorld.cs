a11199e127bb3a0ac4adf9e89076d476eb0e41f0
Andrzej Korolczuk <Andrzej.Korolczuk@king.com>
2017-01-20 14:51:42 +0000

    project setup

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+
+		Debug.Log("hello world");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
