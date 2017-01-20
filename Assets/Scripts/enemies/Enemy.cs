68d214796b6c71046b1c053972cc5e853b54c871
Andrzej Korolczuk <Andrzej.Korolczuk@king.com>
2017-01-20 20:09:42 +0000

    enemies wip, eventdipsatcher

---
+++ Assets/Scripts/enemies/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Enemy : MonoBehaviour 
+{
+	[SerializeField]
+	private Vector3 speed;
+
+	private List<Note> currentChord = new List<Note>();
+
+	private List<Note> killerChord = new List<Note>();
+
+	public void AddActiveNote(Note n)
+	{
+		currentChord.Add(n);
+		CheckKillCondition();
+	}
+
+	public void RemoveActiveNote(Note n)
+	{
+		currentChord.Add(n);
+	}
+
+	private void CheckKillCondition()
+	{
+		
+	}
+}
