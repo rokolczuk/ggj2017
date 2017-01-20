bfe95ad5c4225cefb37d0d7bca5e1842a3e02680
Andrzej Korolczuk <Andrzej.Korolczuk@king.com>
2017-01-20 21:58:16 +0000

    spawning enemies wip

--- Assets/Scripts/enemies/EnemiesFactory.cs
+++ Assets/Scripts/enemies/EnemiesFactory.cs
@@ -2,18 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct EnemySpawnResult
+{
+	public readonly bool HasSpawned;
+
+	public readonly Enemy SpawnedEnemy;
+
+	public EnemySpawnResult(bool spawned, Enemy enemy)
+	{
+		this.SpawnedEnemy = enemy;
+		this.HasSpawned = spawned;
+		
+	}
+}
+
 public class EnemiesFactory : MonoBehaviour 
 {
 	[SerializeField]
-	private EnemySpawnProperties[] enemies;
+	private EnemiesConfiguration configuration;
 
-	// Use this for initialization
-	void Start () 
+
+	public EnemySpawnResult TrySpawn(float timePassed)
 	{
-	}
-		
-	// Update is called once per frame
-	void Update () {
-		
+		for(int i = 0; i < configuration.GetEnemySpawnProperties().Length; i++)
+		{
+			EnemySpawnProperties spawnProperties = configuration.GetEnemySpawnProperties()[i];
+
+			if(spawnProperties.minSpawnTime >= timePassed && Random.Range(0f, 1f) >= spawnProperties.spawnProbability)
+			{
+				GameObject enemyGameObject = GameObject.Instantiate(spawnProperties.prefab) as GameObject;
+				return new EnemySpawnResult(true, enemyGameObject.GetComponent<Enemy>());
+			}
+		}
+
+		return new EnemySpawnResult(false, null);
 	}
 }
---
+++ Assets/Scripts/enemies/EnemiesManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class EnemiesManager: MonoBehaviour
+{
+	[SerializeField]
+	private EnemiesFactory enemiesFactory;
+
+	private float timePassed = 0f;
+
+	private List<Enemy> activeEnemies = new List<Enemy>();
+
+
+	private void SpawnEnemies()
+	{
+		EnemySpawnResult spawnResult = enemiesFactory.TrySpawn(timePassed);
+
+		if(spawnResult.HasSpawned)
+		{
+			Debug.Log("enemy spawned");
+		}
+	}
+
+	public void Update()
+	{
+		timePassed+= Time.deltaTime;
+		SpawnEnemies();
+	}
+}
+
+
