40d892afcdf9bd4ea1889160c7b044500fed64e6
Andrzej Korolczuk <Andrzej.Korolczuk@king.com>
2017-01-21 19:56:18 +0000

    startred skinning 2 note enemy

---
+++ Assets/Scripts/enemies/skinning/TwoNotesEnemySkinProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class TwoNotesEnemySkinProvider : EnemySkinProvider {
+
+	[SerializeField]
+	private List<KeyNote> notes;
+
+	[SerializeField]
+	private List<Sprite> firstNoteSkins;
+
+	[SerializeField]
+	private List<Sprite> secondNoteSkins;
+
+	[SerializeField]
+	private SpriteRenderer firstNoteSpriteRenderer;
+
+	[SerializeField]
+	private SpriteRenderer firstNoteSpriteRenderer;
+
+	public override void SetSkin(Chord killerChord)
+	{
+		Assert.AreEqual(killerChord.notesInChord.Count, 2);
+
+	
+	}
+}
