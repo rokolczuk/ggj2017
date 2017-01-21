c081bca2644375ce091bcc21e328f0203efe86fe
Andrzej Korolczuk <Andrzej.Korolczuk@king.com>
2017-01-21 18:57:28 +0000

    removed debug.log from enemy

--- Assets/Scripts/enemies/Enemy.cs
+++ Assets/Scripts/enemies/Enemy.cs
@@ -63,7 +63,7 @@ public class Enemy : MonoBehaviour
 		if(!currentChord.notesInChord.Contains(n))
 		{
 			currentChord.notesInChord.Add(n);
-			Debug.Log("curr: " + currentChord.ToString() + " / " + killerChord.ToString());
+			//Debug.Log("curr: " + currentChord.ToString() + " / " + killerChord.ToString());
 			dying = hasKillerChord();
 			particles.gameObject.SetActive(dying);
 		}
