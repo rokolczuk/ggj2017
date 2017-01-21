6f7b5e42ff519245ee4310648e1273613c5fa6ce
Kurt Chapman <Kurt.Chapman@king.com>
2017-01-21 13:05:14 +0000

    Changing the audio pool.

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -5,36 +5,21 @@ using System.Text;
 using System;
 
 public class AudioManager : MonoBehaviour {
-
-	const string laserModifier = "_synth";
-	const string keyChangeModifier = "_piano";
 	AudioPool pool;
 
 	// Use this for initialization
 	void Start () {
 		pool = FindObjectOfType<AudioPool> ();
 	}
-
-	TrackName generateTrackname(KeyNoteData note, string modifer) {
-		StringBuilder builder = new StringBuilder (note.ToString ());
-		//builder.Append (note .ToString());
-		builder.Append(modifer);
-		TrackName trackName = (TrackName) Enum.Parse(typeof(TrackName), builder.ToString(), true);
-		return trackName;
+		
+	public void playLaser(KeyNoteData data){
+		pool.playTrack (data.synthSound, true);
 	}
 
-	public void transitionLaser(KeyNoteData currentNote, KeyNoteData newNote){
-		var currentTrackname = generateTrackname (currentNote, laserModifier); 
-		pool.stopTrack (currentTrackname);
-		var newTrackname = generateTrackname (newNote, laserModifier);
-		pool.playTrack (newTrackname, true);
+	public void stopLaser(KeyNoteData data){
+		pool.stopTrack (data.synthSound);
 	}
-
-	public void playLaser(KeyNote note, int octave){
-		//TrackName track = generateTrackname (note, octave, laserModifier);
-		//pool.playTrack (track);
-	}
-	
+		
 	// Update is called once per frame
 	void Update () {
 		
