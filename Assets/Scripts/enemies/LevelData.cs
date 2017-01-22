af5a10bfcf94a7f7e2c820c25e9c44223f4719d6
Kurt Chapman <Kurt.Chapman@king.com>
2017-01-22 13:15:06 +0000

    wave system works and enemy manager resets.

--- Assets/Scripts/enemies/LevelData.cs
+++ Assets/Scripts/enemies/LevelData.cs
@@ -13,9 +13,8 @@ namespace AssemblyCSharp
 		
 	public class LevelData
 	{
-		const int totalWavesIndex = 0;
-		const int amountOfEnemiesSpawnedIndex = 1;
-		const int timeBetweenWavesIndex = 2;
+		const int amountOfEnemiesSpawnedIndex = 0;
+		const int timeBetweenWavesIndex = 1;
 
 		public int amountOfEnemiesSpawned;
 		public float timeBetweenWaves;
@@ -31,24 +30,25 @@ namespace AssemblyCSharp
 			loaData (dataFile);
 		}
 
+		public void reset(){
+			currentWave = -1;
+		}
+
 		void loaData(string file){
 			TextAsset asset = Resources.Load(file) as TextAsset;
 			List<string> splitLines = asset.text.Split ('\n').ToList();
-			totalWaves = int.Parse(splitLines [totalWavesIndex].Split (':') [1].Trim ());
 			amountOfEnemiesSpawned = int.Parse(splitLines [amountOfEnemiesSpawnedIndex].Split (':') [1].Trim ());
 			timeBetweenWaves = float.Parse(splitLines [timeBetweenWavesIndex].Split (':') [1].Trim ());
-			splitLines.RemoveRange (0, 3);
+			splitLines.RemoveRange (0, 2);
 			splitLines  = splitLines.Where(s => !string.IsNullOrEmpty(s)).ToList();
 			foreach(var line in splitLines){
 				EnemyTypes type = (EnemyTypes)int.Parse (line);
 				enemyTypes.Add (type);
 			}
+			totalWaves = enemyTypes.Count / amountOfEnemiesSpawned;
 		}
 
 		public EnemyTypes getNextEnemyType(){
-			if (currentWave >= enemyTypes.Count) {
-				currentWave = 0;
-			}
 			return enemyTypes [currentWave];
 		}
 
