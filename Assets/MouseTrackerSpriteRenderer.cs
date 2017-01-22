f46275a37a672decbf9ed0754afefbc1149f0be7
midasplayer\Edward.James <Edward.James@king.,com>
2017-01-22 10:00:11 +0000

    non local mouse cursor set to low alpha

---
+++ Assets/MouseTrackerSpriteRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MouseTrackerSpriteRenderer : NetworkBehaviour
+{
+	private SpriteRenderer spriteRenderer;
+
+	public override void OnStartAuthority()
+	{
+		base.OnStartAuthority();
+
+		Color color = Color.white;
+		
+		spriteRenderer.color = color;
+	}
+
+	private void Awake()
+	{
+		spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+		Cursor.visible = false;
+
+		Color color = Color.white;
+		color.a = 0.3f;
+
+		spriteRenderer.color = color;
+	}
+}
