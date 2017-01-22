48d83ac066a2d83853572740af9628d736141220
Andrzej Korolczuk <Andrzej.Korolczuk@king.com>
2017-01-22 12:36:28 +0000

    ui fixes

---
+++ Assets/Scripts/UI/GameUIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUIController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		EventDispatcher.AddEventListener<GameStartedEvent>(OnGameStarted);
+		gameObject.SetActive(false);
+	}
+
+	void OnGameStarted(GameStartedEvent e)
+	{
+		gameObject.SetActive(true);
+	}
+
+	private void OnDestroy()
+	{
+		EventDispatcher.RemoveEventListener<GameStartedEvent>(OnGameStarted);
+	}
+}
